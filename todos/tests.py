ed7ecddeabd2e74e84925978521bc589384eba7b
Arets Paeglis <arets.paeglis@protonmail.com>
2023-08-27 12:46:20 +0300

    Initial commit for the SDA Django REST API ToDo app

---
+++ todos/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from .models import Todo
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(title='first todo', body='a body here')
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, 'first todo')
+        self.assertEqual(self.todo.body, 'a body here')
+        self.assertEqual(str(self.todo), 'first todo')
